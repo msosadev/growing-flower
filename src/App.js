4b4a613c19fd9dcad00552050c38bd2776486174
Miguel Sosa <msosadesign@gmail.com>
2025-01-18 14:32:33 +0100

    Set special flower when opening on specific date

--- src/App.js
+++ src/App.js
@@ -3,15 +3,28 @@ import Flower from './components/Flower';
 import useRunningTime from './hooks/useRunningTime';
 import pot from './images/pots/pot_1.svg';
 import colors from './colors.json';
+import { useState } from 'react';
 
 function generateRandomValue() {
   return Math.floor((Math.random() * 8) + 1)
+}
+
+function checkDate(month, day) {
+  // Get today's date
+  const today = new Date();
+
+  if (today.getMonth() === month && today.getDate() === day) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // START ------------------------------------------------------------------
 
 function App() {
   const runningTime = useRunningTime();
+  const [specialFlower, setSpecialFlower] = useState(false);
   let missingItems;
   const flowersKey = "flowers"
   const palettesKey = "palettes"
@@ -19,15 +32,15 @@ function App() {
   let savedPalettes = [];
   const flowersToRender = Math.floor(runningTime / 60);
 
-  const renderFirstFlower = flowersToRender > 0;
+  // const renderFirstFlower = flowersToRender > 0;
 
-  if (renderFirstFlower && !localStorage.getItem(flowersKey)) {
-    localStorage.setItem(flowersKey, generateRandomValue());
+  if (!localStorage.getItem(flowersKey)) {
+    // If it's January 19 set special flower ixora
+    checkDate(0, 19) ? localStorage.setItem(flowersKey, "ixora") : localStorage.setItem(flowersKey, generateRandomValue());
     localStorage.setItem(palettesKey, generateRandomValue());
-    savedFlowers = localStorage.getItem(flowersKey).split(",");
-    savedPalettes = localStorage.getItem(palettesKey).split(",");
+    setSpecialFlower(true);
     missingItems = 0;
-  } else if(localStorage.getItem(flowersKey)) {
+  } else if (localStorage.getItem(flowersKey)) {
     savedFlowers = localStorage.getItem(flowersKey).split(',');
     savedPalettes = localStorage.getItem(palettesKey).split(",");
     missingItems = flowersToRender - savedFlowers.length;
@@ -41,7 +54,6 @@ function App() {
     localStorage.setItem(palettesKey, savedPalettes);
   }
 
-
   return (
     <div className="App flex flex-col-reverse h-screen overflow-y-scroll pb-8">
       <div className="flex flex-col items-center relative">
@@ -50,7 +62,7 @@ function App() {
         </div>
 
         {savedFlowers.map((flowerIndex, index) => {
-          if ((index + 1 ) <= flowersToRender) {
+          if ((index + 1) <= flowersToRender) {
             return <Flower key={index} index={index} flowerIndex={flowerIndex} runningTime={runningTime} palette={colors[savedPalettes[index]]} />
           }
         })}
