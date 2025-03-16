e5418f7591aaff84d794948e8da35aee155ce03d
Miguel Sosa <msosadesign@gmail.com>
2025-03-16 21:53:37 +0100

    Remove unnecessary code

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,38 +1,22 @@
-import React, { useRef, useState } from 'react';
-
 function Settings(props) {
-    const [isDialogOpen, setIsDialogOpen] = useState(true);
-    const dialog = useRef();
-
-    const toggleDialog = () => {
-        setIsDialogOpen(!isDialogOpen);
-        if (isDialogOpen) {
-            dialog.current.showModal();
-        } else {
-            dialog.current.close();
-        }
-    };
-
     return (
-        <>
-            <div>
-                <h2>Window background:</h2>
-                <div className="flex items-center">
-                    <div className='flex-1'>
-                        <input
-                            type="file"
-                            accept="image/*"
-                            onChange={props.handleImageChange}
-                            className="hidden"
-                            id="window-background-input"
-                        />
-                        <label className="px-2 w-fit py-1 block bg-purple-500 text-white rounded-md text-sm hover:bg-purple-400 transition-colors cursor-pointer" htmlFor="window-background-input">Set Image</label>
-                    </div>
-
-                    <div style={{ backgroundImage: `url(${props.selectedImage})` }} className='bg-center bg-cover aspect-square min-h-8'></div>
+        <div>
+            <h2>Window background:</h2>
+            <div className="flex items-center">
+                <div className='flex-1'>
+                    <input
+                        type="file"
+                        accept="image/*"
+                        onChange={props.handleImageChange}
+                        className="hidden"
+                        id="window-background-input"
+                    />
+                    <label className="px-2 w-fit py-1 block bg-purple-500 text-white rounded-md text-sm hover:bg-purple-400 transition-colors cursor-pointer" htmlFor="window-background-input">Set Image</label>
                 </div>
+
+                <div style={{ backgroundImage: `url(${props.selectedImage})` }} className='bg-center bg-cover aspect-square min-h-8'></div>
             </div>
-        </>
+        </div>
     )
 }
 
